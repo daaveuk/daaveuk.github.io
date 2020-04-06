7679bff481f0027be9cb593ddb8841043b604b8e
David Henderson <me@daave.co.uk>
2020-04-06 11:14:28 +0100

    Updates

---
+++ precache-manifest.82c82de11465a8af875a96ca7f29a0f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d75725705d8fe9758fef46cb231f4c60",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b8d938a0062d70e748d",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "dc70f66238a6abbabd12",
+    "url": "/static/js/2.77573df0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.77573df0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b8d938a0062d70e748d",
+    "url": "/static/js/main.5a1ec7b9.chunk.js"
+  },
+  {
+    "revision": "f22f9ae2a7367fa581a1",
+    "url": "/static/js/runtime-main.e4ca2e0d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
