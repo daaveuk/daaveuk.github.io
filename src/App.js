a6566dc40ff151ddac504bf761d32e135d21adcb
David Henderson <me@daave.co.uk>
2020-04-10 11:16:25 +0100

    Wrong direction

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
 import React from 'react';
 import './App.css';
+import GeoPattern from 'geopattern';
+import randomColor from 'randomcolor';
 
 function App() {
-  return <div className="App">Hello, World</div>;
+  const color = randomColor();
+  const d = new Date();
+  const n = d.getTime();
+  const pattern = GeoPattern.generate(`${n}`, {
+    color,
+  });
+  return (
+    <div className="App">
+      <div
+        className="bg"
+        style={{ backgroundImage: `url(${pattern.toDataUri()})` }}
+      />
+      <div className="deets">
+        <h1 style={{ color }}>
+          David Henderson <br />
+          Web Developer
+        </h1>
+        <p>
+          I create awesome products with Javascript, HTML and CSS. Currently
+          living a breathing React at kaboodle. Don't be a stranger! Get in
+          touch here.
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default App;
