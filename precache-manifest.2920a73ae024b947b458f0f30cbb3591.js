391ac2193f2bda1a1a68c658a557a15b07d2886b
daaveuk <me@daave.co.uk>
2020-04-28 10:38:01 +0000

    Deploying to master from  @ 00c23802b8a65ae72070d42d4d60b9fb003768a8 🚀

---
+++ precache-manifest.2920a73ae024b947b458f0f30cbb3591.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b2bf1e5a82e6b7741509de176783980",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bd1c70826dfb2173b8c5",
+    "url": "/static/js/2.b8ed99c7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b8ed99c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00cec0c6bb28f82d5512",
+    "url": "/static/js/main.9e77b7be.chunk.js"
+  },
+  {
+    "revision": "a810084d49a7041bf754",
+    "url": "/static/js/runtime-main.61ae2d7a.js"
+  }
+]);
